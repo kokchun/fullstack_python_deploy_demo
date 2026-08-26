c64db8f38a31fd5486d81021e10e2f258f87bb80
kokchun <kokchungiang@gmail.com>
2026-08-26 14:33:40 +0200

    dataframe

--- frontend/src/frontend/dashboard.py
+++ frontend/src/frontend/dashboard.py
@@ -5,8 +5,8 @@ import httpx
 def main():
     st.markdown("# PokeDash")
 
-    stats = httpx.get("http://127.0.0.1:8000/pokemons/stats", timeout=30)
-    st.write(stats)
+    stats = httpx.get("http://127.0.0.1:8000/pokemons/stats", timeout=30).json()
+    st.dataframe(stats)
 
 if __name__ == "__main__":
     main()
